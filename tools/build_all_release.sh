9bebbe2818a24e28120e95ec476010bdef918b8b
smirnov-yv <smirnov-yv@neo-s.ru>
2026-01-31 22:54:19 +0300

    Add task for clangd

--- tools/build_all_release.sh
+++ tools/build_all_release.sh
@@ -10,6 +10,5 @@ find "$start_dir" -type f -name 'CMakeLists.txt' | while IFS= read -r cmake_file
         cd "$proj_dir" || exit 1
         cmake -S . -B build -DCMAKE_BUILD_TYPE=Release &&
         cmake --build build
-        cp ./build/compile_commands.json ${start_dir}/.compile_commands/compile_commands_$(basename "$proj_dir").json
     )
 done
