72325468a593a69d225a837987bbd9910d1a73cf
smirnov-yv <smirnov-yv@neo-s.ru>
2026-02-11 16:40:13 +0300

    migrate to gitlab

--- tools/updateDepsList.sh
+++ tools/updateDepsList.sh
@@ -3,7 +3,8 @@
 workspace=$(realpath "$(dirname "$(realpath $0)")/../../..")
 cd ${workspace}/PluginsSource
 rm -f list
-wget -q https://raw.githubusercontent.com/d3156/PluginCore_SourcesList/main/list
+
+wget -q "https://gitlab.bubki.zip/d3156/PluginCore_SourcesList/-/raw/main/list" -O list
 
 while IFS= read -r repo; do
     [ -z "$repo" ] && continue
